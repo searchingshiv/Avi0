85f1cc7453a7533bbf2ca3e23272c3ed8ddf8232
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2023-12-06 15:13:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,21 +10,17 @@ __I Am Auto CaptionBot Just Add In Channel and See Magic__
 
 __ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ ғᴏʀ ᴀ ᴄʜᴀɴɴᴇʟ ᴜsᴇ ᴍᴇ__
 
-😈 __ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ__ 👉 __@{}__
+😈 **ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 [𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧](https://t.me/THE_DS_OFFICIAL)**
 """    
 
       HELP_TEXT = """
 <u>💡 𝐇𝐞𝐥𝐩</u> 
 
 👉 <i>Add me as a Admin in your channel with edit permission</i>
-
-👉 <i>Add your caption in heroku Config Var</i>
    
 <i>[Support Markdown]</i>
 
 👉 <i>Forward your files in your channel and I will edit it</i>
-
-○ <i>My Source Code : /source</i>
 
 """    
               
@@ -33,33 +29,33 @@ __ɪ ᴀᴍ ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴏʀᴋɪɴɢ ғᴏʀ ᴀ ᴄʜᴀɴɴᴇ
       ABOUT_TEXT = """
 📕 **𝐀𝐛𝐨𝐮𝐭 𝐌𝐞**
 
-__○ ᴍʏ ɴᴀᴍᴇ : [CapXbot](https://t.me/Avishkarpatil)__
+__○ ᴍʏ ɴᴀᴍᴇ : {}
 __○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ __
 __○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ __
 __○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ __
 __○ ᴠᴇʀsɪᴏɴ : 2.0.1__
-__○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
+__○ ᴄʀᴇᴀᴛᴏʀ :  [𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧](https://t.me/THE_DS_OFFICIAL)
  
-**[© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ](https://t.me/Avishkarpatil)**
+**[© Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ](https://t.me/THE_SILENT_TEAMS)**
 """
 
       MARKDOWN_TEXT = """
 🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
 👉 <b>Bold text</b>
-🔸 <code>**Avishkar**</code>
+🔸 <code>**Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ**</code>
 
 👉 <b>Italic text</b>
-🔹 <code>__Avishkat__</code> 
+🔹 <code>__Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ__</code> 
 
 👉 <b>Code text</b>
-🔸 <code>`Avishkar`</code>   
+🔸 <code>`Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ`</code>   
 
 👉 <b>Hyperlink text</b>
-🔹 <code>[hyperlink_text](https://avipatilweb.me)</code> 
+🔹 <code>[hyperlink_text](https://t.me/THE_SILENT_TEAMS)</code> 
 
 〰〰〰〰〰〰〰〰〰〰
 
-<b><a href="https://t.me/Avishkarpatil">© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ</a></b>
+<b><a href="https://t.me/THE_SILENT_TEAMS">© Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ</a></b>
 """
 
 # Bot status display
@@ -72,16 +68,11 @@ __○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
 
 📐 <b>Current Position :</b> {}
 
-<b><a href="https://t.me/Avishkarpatil">© ᴀᴠɪsʜᴋᴀʀ ᴘᴀᴛɪʟ</a></b>
+<b><a href="https://t.me/THE_SILENT_TEAMS">© Tʜᴇ Sɪʟᴇɴᴛ Tᴇᴀᴍ</a></b>
 """
 
 
       SOURCE_TEXT = """
 
-○ <b> I Am Available Open Source on Github 
-      Click Below Link And Deploy Me Now </b>
-
-○ <i>DEPLOY</i> : <b><a href="https://heroku.com/deploy?template=https://github.com/avipatilpro/Caption-Bot">On Heroku</a></b>    
-
-○ <i>SOURCE</i> : <b><a href="https://github.com/avipatilpro/Caption-Bot">Caption Bot</a></b>  
+○ <b>Cʜᴀʟᴀ Jᴀ BSDK</b>  
 """

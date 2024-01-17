65bbb9035aace4f23a946aa3f3882d416c83c537
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2024-01-17 19:41:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ class autocaption(Client):
     
     def __init__(self):
         super().__init__(
-            session_name="Caption",
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,

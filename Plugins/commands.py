91a14971e30fd3372a502df8363332f9a5382784
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-19 20:58:31 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -75,7 +75,7 @@ source_button=InlineKeyboardMarkup(
 async def start(bot, cmd):
       await bot.send_message(
           chat_id = cmd.chat.id,
-          text = Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME), 
+          # text = Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME), 
           reply_to_message_id=cmd.message.message_id if cmd.message else None,
           parse_mode = "markdown",
           disable_web_page_preview = True, 

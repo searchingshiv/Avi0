d8bae0c7e83a4c4ea0832af53e8d805475fe2ff6
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-19 18:55:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import pyrogram
 
 import logging
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.WARNING,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 

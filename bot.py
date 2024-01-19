13b87c4d30f74148835348767c34ccb1e97bc881
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-20 01:24:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,16 +20,16 @@ logging.getLogger("pyrogram").setLevel(logging.DEBUG)
 class autocaption(Client):
     
     def __init__(self):
-        super().__init__(
+        super().__init__{
             session_name="Unknown",
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,
             workers = 20,
-            plugins = dict(
+            plugins = dict{
                 root="Plugins"
-            )
-        )
+            }
+      }
 if __name__ == "__main__" :
     autocaption().run()
 

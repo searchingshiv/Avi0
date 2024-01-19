56a417234f67dd374f26e0efca0cce0e23df40f1
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-19 21:15:40 +0530

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -4,7 +4,7 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 import asyncio
-from pyrogram import filters,Client
+from pyrogram import filters
 from bot import autocaption
 from config import Config
 
@@ -15,7 +15,7 @@ caption_position = usercaption_position.lower()
 caption_text = Config.CAPTION_TEXT
 
 
-@autocaption.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~~filters.forwarded, group=-1)
+@autocaption.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
 async def editing(bot, message):
       try:
          media = message.document or message.video or message.audio
@@ -34,21 +34,21 @@ async def editing(bot, message):
       try:
           if caption_position == "top":
              await bot.edit_message_caption(
-                 reply_to_message_id=cmd.message_id, 
+                 chat_id = message.chat.id, 
                  message_id = message.message_id,
                  caption = caption_text + "\n" + file_caption,
                  parse_mode = "markdown"
              )
           elif caption_position == "bottom":
              await bot.edit_message_caption(
-                 reply_to_message_id=cmd.message_id, 
+                 chat_id = message.chat.id, 
                  message_id = message.message_id,
                  caption = file_caption + "\n" + caption_text,
                  parse_mode = "markdown"
              )
           elif caption_position == "nil":
              await bot.edit_message_caption(
-                 reply_to_message_id=cmd.message_id,
+                 chat_id = message.chat.id,
                  message_id = message.message_id,
                  caption = caption_text, 
                  parse_mode = "markdown"

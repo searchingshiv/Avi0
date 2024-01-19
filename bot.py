d33144b68612731fae3e2c580fa1bbb6d9d39efe
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-01-19 17:46:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class autocaption(Client):
     
     def __init__(self,name):
         super().__init__(
-            self.name = name
+            self.name == name
             bot_token = Config.BOT_TOKEN,
             api_id = Config.API_ID,
             api_hash = Config.API_HASH,
